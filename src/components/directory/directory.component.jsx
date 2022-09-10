0cb0241611c187766165876d06d5c9826f121eac
ltchelishvili1 <tchelishvililevani@gmail.com>
2022-09-10 12:55:32 +0400

    eccomerce

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import CategoryItem from '../categories/category-item.component'
+
+import './categories.styles.scss'
+
+const Directory = ({categories}) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+
+      ))}
+
+
+    </div>
+  )
+}
+
+export default Directory
